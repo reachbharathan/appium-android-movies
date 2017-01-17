1fe035c76d2b59a30454c0342b3fbe032a9172af
Bharathan Kumaran <reachbharathan@gmail.com>
2017-01-17 13:43:09 +0530

    Adding sample test to test testng working

---
+++ src/test/java/SampletTestngTest.java
@@ -0,0 +1,20 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Package name PACKAGE_NAME
+ * Created by Bharathan on 17/01/17.
+ * Project Name appium-android-calculator
+ */
+public class SampletTestngTest {
+
+  @Test
+  public void sampleTestPositive() {
+    Assert.assertTrue(true);
+  }
+
+  @Test
+  public void sampleTestNegative() {
+    Assert.assertTrue(false);
+  }
+}
