04302f932644026c36b2ec60e044a0e836d9ec5f
Bharathan Kumaran <reachbharathan@gmail.com>
2017-02-07 14:47:36 -0500

    Adding the BitbarSampleApp.apk to the test resourcesz

--- startAppium.sh
+++ startAppium.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-appium --address "127.0.0.1" --session-override --pre-launch --debug-log-spacing --default-capabilities '{"platformName":"Android","platformVersion":"5.1","automationName":"Appium","deviceName":"Nexus_5_API_22","app":"/Users/Bharathan/Library/Android/sdk/tools/BitbarSampleApp.apk"}'
+appium --address "127.0.0.1" --session-override --pre-launch --debug-log-spacing --default-capabilities '{"platformName":"Android","platformVersion":"5.1","automationName":"Appium","deviceName":"Nexus_5_API_22","app":"src/test/resources/BitbarSampleApp.apk"}'
